748b2eaf0d3133268d90d25f49d0f4d6cb439dd1
LiigaLi <112900363+LiigaLi@users.noreply.github.com>
2022-09-26 21:39:56 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Mani sauc Liga");
+        System.out.println("Esmu Liga");
+    }
+
+
+}
---
+++ src/main/java/lekcijaViens/labDarbs/Main.java
@@ -0,0 +1,119 @@
+package lekcijaViens.labDarbs;
+
+public class Main {
+    public static void main(String[] args) {
+        //int - integer, satur veselu skaitli
+        int vecums = 34;
+
+        int temperatura = -10;
+        System.out.println("Ziemā būs: " + temperatura);
+
+        System.out.println("Mans vecums ir:" + vecums);
+        System.out.println(vecums) ;
+
+        int garums = 180;
+        System.out.println("Mans garums ir:" + garums) ;
+        int kurpjuizmers = 44;
+        System.out.println("Mans kurpju izmers ir:" + kurpjuizmers) ;
+        int darbaStazs = 5;
+        System.out.println("Mans darba stazs ir:" + darbaStazs) ;
+        int svars = 80;
+        System.out.println("Mans svars ir:" + svars) ;
+
+
+        System.out.println("VECUMS:" + vecums + "\nGARUMS: " + garums + "\nKurpju izmers: " + kurpjuizmers);
+
+        //Maksimālā integer vērtība
+        int maxInt = 2147483647;
+
+        //Minimālā integer vērtība
+        int minInt = -2147483648;
+
+        //byte pieņem veselu skaitli no -127 līdz +127
+        byte vecumsDivi = 99;
+
+        short latvijasMazpilseta = 17000;
+
+        long zivisOkeana = 2147486349999L;
+
+        float apavuIzmers = 44.5F;
+        System.out.println("apavu izmers" + apavuIzmers);
+
+        double grauduSvars = 150.3;
+        System.out.println("Tika pārdoti " + grauduSvars + "kg graudi");
+
+        //Šodien ir setsdiena, lielveikals ir atvērts
+        boolean isSuperMarketOpen = true;
+
+        //Turpretim skolas ir slēgtas
+        boolean isSchoolClosed = false;
+
+        String sunaVards = "Lote";
+        String teksts = "Suņa vārds ir: ";
+        System.out.println("Suņa vārds ir: " + sunaVards);
+
+        String sunaSkirne = "VAS";
+        char valuta = '$';
+
+        char dzimums = 'V';
+        char atzime = 'F';
+
+
+     System.out.println(valuta + atzime + dzimums);
+
+System.out.println("test" + valuta + atzime + dzimums);
+        int a = 5;
+        int b = 10;
+        int summa;
+        summa = a + b;
+
+
+        System.out.println(summa);
+
+        summa = a + a;
+
+        System.out.println(summa);
+
+        String name = "Juris";
+        System.out.println(name);
+        name = "Jānis";
+        System.out.println(name);
+
+        int x = 10;
+        int y = -45;
+
+        int rezultats = x + y;
+        System.out.println(rezultats);
+
+        rezultats = x - y;
+        System.out.println(rezultats);
+
+        rezultats = x * y;
+        System.out.println(rezultats);
+
+        rezultats = x/y;
+        System.out.println(rezultats);
+
+        rezultats = 10 / 2;
+        System.out.println(rezultats);
+
+        rezultats = 11 % 2;
+        System.out.println(rezultats);
+
+        rezultats = 13 % 2;
+        System.out.println(rezultats);
+
+        rezultats = 5*(3+7)/2;
+        System.out.println(rezultats);
+
+
+
+
+
+
+
+
+
+
+    }
+}
